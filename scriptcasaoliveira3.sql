4e899217791f0987920eeb8a765b3f48c57389f7
Miqueias Rodrigues <miqueiasr560@gmail.com>
2024-04-30 17:10:55 -0300

    add

---
+++ scriptcasaoliveira3.sql
@@ -0,0 +1,10 @@
+/*
+select cliente.nomecliente,
+	   venda.total,
+       produto.nomeproduto
+from cliente,venda,produto;
+*/
+
+select * from endereco;
+select * from endereco where tipologradouro="Rua";
+select count(idendereco) from endereco;
